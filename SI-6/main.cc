7332920fd3656d43becd4671ab9f2e2c0cab62a1
Kait Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-09-18 13:39:51 -0700

    Update main.cc

--- SI-6/main.cc
+++ SI-6/main.cc
@@ -1,1 +1,20 @@
+#include <iostream>
 
+#include "vendingmachine.h"
+
+
+int main
+{
+  // declare variables
+  
+  // prompt user for initial vending machine states and customer money states
+
+  // construct vending machine called tuffys_vm
+
+  // ask user if they would like to purchase anything
+  // if yes, prompt user for how many
+  // process users request
+
+  // print updated values
+
+}
