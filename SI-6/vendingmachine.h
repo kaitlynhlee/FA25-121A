daa1ad5877bfff64e5859becb8d3779a2d0abbc7
Kait Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-09-18 11:35:32 -0700

    Update vendingmachine.h

--- SI-6/vendingmachine.h
+++ SI-6/vendingmachine.h
@@ -1,1 +1,17 @@
+#include <string>
 
+class VendingMachine {
+ public:
+  // Member functions of the VendingMachine class.
+  // These member functions are called "setters" or "setter functions"
+  // because they SET the member variables to the given input.
+
+
+  // These member functions are called "getters" or "getter functions"
+  // because they GET, or return, the member variables to the given input.
+
+
+ private:
+  // Member variables of the VendingMachine class.
+
+};
