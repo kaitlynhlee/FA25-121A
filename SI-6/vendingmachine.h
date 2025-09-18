67a2c437cfc1d499a992a03e6b20655c76631e4d
Kait Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-09-18 13:33:16 -0700

    Update vendingmachine.h

--- SI-6/vendingmachine.h
+++ SI-6/vendingmachine.h
@@ -1,5 +1,13 @@
 #include <string>
 
+//In this file, you should declare your class VendingMachine; which should:
+// hold the number of items in the machine, the cost of a single item, and the amount collected from sales
+// it should also:
+// be initialized with the number of items, the starting price, and the initial sales to zero,
+// be able to display the current state of the machine (items, price, and sales)
+// be able to handle purchase requests, modifying the customer's money, machine's inventory,
+// and the amount made in sales; also printing a message upon transaction failure (conditions not being met)
+
 class VendingMachine {
  public:
   // Member functions of the VendingMachine class.
